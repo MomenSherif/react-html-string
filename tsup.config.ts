b54540dadabfae7b83163c96c26ea2015b096891
Mo'men Sherif <momensherif.2019@gmail.com>
2022-09-03 14:48:30 +0200

    initial setup

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup';
+import { dependencies, peerDependencies } from './package.json';
+
+export default defineConfig(options => ({
+  entry: ['src/index.tsx'],
+  outDir: 'dist',
+  sourcemap: false,
+  clean: true,
+  dts: !options.watch,
+  format: options.watch ? 'esm' : ['esm', 'cjs'],
+  external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
+  outExtension({ format }) {
+    console.log({ format });
+    return {
+      js: `.${format}.js`,
+    };
+  },
+  minify: !options.watch,
+}));
